4a6f7e82f7abc8a6bd1bb07b190fee68d3ff42b6
ulrikagoloconda <golocondafahlen@gmail.com>
2017-05-18 08:16:14 +0200

    Init commit

---
+++ test.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Ulrika Goloconda Fahlen
+      * Date: 2017-05-18
+      * Purpose: training
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TRAINING.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Mitt första program"
+            STOP RUN.
+       END PROGRAM TRAINING.
